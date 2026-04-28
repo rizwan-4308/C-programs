81cc4ce4579100ff268e44e6ac5d1dc5545a28b3
rizwan-4308 <rizwan4308@gmail.com>
2026-04-28 12:15:18 +0530

    Initial commit

---
+++ 01_Variables/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char characterName[] = "Tom";
+    int characterAge = 65;
+    printf("There once was a man named %s\n", characterName);
+    printf("he was %d years old.\n", characterAge);
+    printf("he really liked the name %s\n", characterName);
+    printf("but he does not like being %d.\n", characterAge);
+
+    return 0;
+
+}
