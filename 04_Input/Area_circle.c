43cfd57efb06baf1cc5f0c80495760eee3b0689b
rizwan-4308 <rizwan4308@gmail.com>
2026-04-30 12:31:58 +0530

    Area of circle

---
+++ 04_Input/Area_circle.c
@@ -0,0 +1,11 @@
+# include<stdio.h>
+
+// Area of Circle
+int main() {
+    float radius;
+    printf("Ente radius: ");
+    scanf("%f", &radius);
+
+    printf("Area of Circle: %f", 3.14 * radius * radius);
+    return 0;
+}
