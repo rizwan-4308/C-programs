e4f60d108bad067c8fe41aa909a0b345a4103686
rizwan-4308 <rizwan4308@gmail.com>
2026-04-30 12:28:29 +0530

    Area of square

---
+++ 04_Input/Area.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+// Area of a square
+int main() {
+    
+    float side;
+    printf("Entaer side: ");
+    scanf("%f", &side);
+
+    printf("Area is: %f", side * side);
+    return 0;
+}
