512a36c106e6bf6c7862083d5d52a5c07762cd8b
rizwan-4308 <rizwan4308@gmail.com>
2026-04-30 12:18:02 +0530

    Addition

---
+++ 04_Input/Addition.c
@@ -0,0 +1,13 @@
+# include<stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter A:");
+    scanf("%d", &a);
+
+    printf("Enter B: ");
+    scanf("%d", &b);
+
+    printf("Sum = %d", a + b);
+    return 0;
+}
