25c734013de1aa7162cfc12c55a39be00bf2c35f
rizwan-4308 <rizwan4308@gmail.com>
2026-05-06 13:53:45 +0530

    Assignment Operators

---
+++ 05_Operators/Assignment_Operators.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int a = 1;
+    int b = 4;
+    // a = a + b;
+    a += b;
+    a -= b;     // a = a - b;
+    a *= b;     // a = a * b;
+    a /= b;     // a = a / b;
+
+    printf("%d \n", a);
+
+
+    return 0;
+}
